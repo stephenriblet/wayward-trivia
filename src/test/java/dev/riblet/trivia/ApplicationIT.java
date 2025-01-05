b5d70fe0fb9bbdb65dcc1d60cdcfa18f22af4433
Stephen Riblet <stephenriblet@gmail.com>
2025-01-05 16:28:31 -0500

    Initial commit

---
+++ src/test/java/dev/riblet/trivia/ApplicationIT.java
@@ -0,0 +1,13 @@
+package dev.riblet.trivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
